e8719af209ff253b3d9481c9ffdb4aff729e3bdb
zrida1 <ragimovazrida@gmail.com>
2026-08-02 16:46:55 +0400

    implement Category entity with JPA mappings

--- src/main/java/com/example/moviewreviewapplication/entity/Category.java
+++ src/main/java/com/example/moviewreviewapplication/entity/Category.java
@@ -1,9 +1,21 @@
 package com.example.moviewreviewapplication.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Entity
+@Getter
+@Setter
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String name;
+    @ManyToMany(mappedBy = "categories")
     private List<Movie> movies;
 }
