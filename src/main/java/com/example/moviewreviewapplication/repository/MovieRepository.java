9a54ab5f08e12f9dd60978b4fd2532cc3174c7fe
zrida1 <ragimovazrida@gmail.com>
2026-08-03 18:06:02 +0400

    add Movie repository JPQL

--- src/main/java/com/example/moviewreviewapplication/repository/MovieRepository.java
+++ src/main/java/com/example/moviewreviewapplication/repository/MovieRepository.java
@@ -2,6 +2,8 @@ package com.example.moviewreviewapplication.repository;
 
 import com.example.moviewreviewapplication.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,20 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     List<Movie> findByReleaseYear(Integer releaseYear);
     List<Movie> findByImdbRatingGreaterThanEqual(Double rating);
     List<Movie> findByTitleContainingIgnoreCase(String title);
+    @Query("""
+        SELECT m
+        FROM Movie m
+        WHERE m.imdbRating >= :rating
+        AND m.releaseYear >= :year
+    """)
+    List<Movie> findTopMovies(@Param("rating") Double rating, @Param("year") Integer year);
+
+    @Query("""
+        SELECT m
+        FROM Movie m
+        JOIN m.categories c
+        WHERE c.name = :categoryName
+    """)
+    List<Movie> findMoviesByCategory(@Param("categoryName") String categoryName);
+
 }
