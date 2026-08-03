827d445183cfc9401f7daacbb9b90b4ff13b2c14
zrida1 <ragimovazrida@gmail.com>
2026-08-03 18:04:14 +0400

    add Movie repository derived query

--- src/main/java/com/example/moviewreviewapplication/repository/MovieRepository.java
+++ src/main/java/com/example/moviewreviewapplication/repository/MovieRepository.java
@@ -4,6 +4,12 @@ import com.example.moviewreviewapplication.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findByGenre(String genre);
+    List<Movie> findByReleaseYear(Integer releaseYear);
+    List<Movie> findByImdbRatingGreaterThanEqual(Double rating);
+    List<Movie> findByTitleContainingIgnoreCase(String title);
 }
